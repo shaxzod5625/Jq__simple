32a7abae39c053fe93dc2cce9a4d29d1aba4da77
shaxzod5625 <76556205+shaxzod5625@users.noreply.github.com>
2021-03-13 18:40:50 +0500

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -47,3 +47,50 @@ $('.read-more').on('click', function () {
         $('.read-more').html('Read More');
     }
 })
+
+$('.js-modal-show').click(function (e) {
+    e.preventDefault();
+    $('.modal').fadeIn(500);
+})
+
+$('.modal .close').click(function (e) {
+    e.preventDefault();
+    $('.modal').fadeOut(500);
+})
+
+$('.nav').css({
+    position: 'fixed',
+    top: $(window).height() - $('.nav').outerHeight() + 'px',
+    width: 100 + '%',
+    zIndex: 10
+})
+
+$(window).on('scroll', function () {
+    let editPosition = $(window).height() - $('.nav').outerHeight() - $(window).scrollTop();
+    if (editPosition > 0) {
+        $('.nav').css({
+            top: editPosition + 'px'
+        })
+    } else {
+        $('.nav').css({
+            top: 0
+        })
+    }
+})
+
+$(window).on('scroll', function () {
+    let btnTop = $(window).scrollTop() - 100
+    console.log(btnTop);
+    if (btnTop >= 0) {
+        $('.js-btn-top').css({
+            bottom: 0
+        })
+    } else {
+        $('.js-btn-top').css({
+            bottom: -100 + '%'
+        })
+    }
+})
+$('.btn').click(function(){
+    $('html, body').animate({scrollTop: 0}, 1000);
+});
